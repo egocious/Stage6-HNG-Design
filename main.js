ae45ed2827a04cbc41b147752913a3e157689e61
Ego <47955681+egocious@users.noreply.github.com>
2019-10-09 19:54:54 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,232 @@
+mentorsArr = [
+ {
+   name: "Oluwatoni Atunrase",
+   stack: "Web Developer",
+   imgUrl: link to image file
+ },
+ {
+    name: "Patrick Aziken",
+    stack: "Software Tester",
+    imgUrl: link to image file
+  },
+  {
+    name: "Stephen Azubuike",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Everitus Olumese",
+    stack: "Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Osumgha Chiamaka",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Aboagye",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Ademoye",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Adeyefa Oluwatoba",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Adolphus Chris",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Ogundiji Bolade",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Ajayi Jeremiah",
+    stack: "Backend Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Albert Oboh",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Amujo Oluwasomido",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Chigozie",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Kev Chike",
+    stack: "Software Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Chinedu Ebinim",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Chukwuemeka Iheonye",
+    stack: "Product Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Somtozech",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Cole Solomon",
+    stack: "Product Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Motunrayo Da-costa",
+    stack: "Frontend Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Maaruf Dauda",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: link to image file
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: link to image file
+  },
+   {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: link to image file
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: link to image file
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: link to image file
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: link to image file
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: link to image file
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: link to image file
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: link to image file
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: link to image file
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: link to image file
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: link to image file
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: link to image file
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: link to image file
+  },
+]
+
+
+
+
+
+
+
+
+
+
